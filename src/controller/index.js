ec5272f0fd44bbd4eb8c6e667b06fd1d1204f2bd
Llewellyn D'souza <lledsouza2209@gmail.com>
2021-12-22 17:40:14 +0530

    Added: row

--- src/controller/index.js
+++ src/controller/index.js
@@ -5,6 +5,7 @@ const warehouseRouter = require("./warehouse.router");
 const zoneRouter = require("./zone.router");
 const areaRouter = require("./area.router");
 const bayRouter = require("./bay.router");
+const rowRouter = require("./row.router");
 
 router.use("/user", userRouter);
 router.use("/company", companyRouter);
@@ -12,6 +13,7 @@ router.use("/warehouse", warehouseRouter);
 router.use("/zone", zoneRouter);
 router.use("/area", areaRouter);
 router.use("/bay", bayRouter);
+router.use("/row", rowRouter);
 
 router.get("/", (req, res) => {
   res.send("Hello world");
