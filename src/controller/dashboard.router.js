30fddfc8fd806e4d1daeaa211d5dbd9a66e347e7
Llewellyn D'souza <lledsouza2209@gmail.com>
2022-02-16 15:41:27 +0530

    Added: get children from parent w/ id, type

--- src/controller/dashboard.router.js
+++ src/controller/dashboard.router.js
@@ -11,4 +11,9 @@ router.post("/create-warehouse-schema", controller.createWarehouseSchema);
  */
 router.post("/create-inventory-schema", controller.createInventorySchema);
 
+/**
+ * @route /dashboard/get-children-from-parent
+ */
+router.post("/get-children-from-parent", controller.getChildrenFromParent);
+
 module.exports = router;
